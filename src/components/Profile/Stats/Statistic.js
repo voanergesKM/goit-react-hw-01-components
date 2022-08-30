00a0d7c2d69b2d54d87e8eb49a830f53957fe43b
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-30 18:25:13 +0300

    Profile update

--- src/components/Profile/Stats/Statistic.js
+++ src/components/Profile/Stats/Statistic.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { StyledContainer, StatsUnit, StatItem } from './Statistic.styled';
 
 export const Statistic = ({ followers, views, likes }) => {
@@ -5,16 +6,34 @@ export const Statistic = ({ followers, views, likes }) => {
     <StyledContainer>
       <StatsUnit>
         <StatItem>Followers</StatItem>
-        <StatItem accent>{followers}</StatItem>
+        <StatItem accent>
+          {followers.toString().slice(0, -3) +
+            `,` +
+            followers.toString().slice(-3, followers.length)}
+        </StatItem>
       </StatsUnit>
       <StatsUnit>
         <StatItem>Views</StatItem>
-        <StatItem accent>{views}</StatItem>
+        <StatItem accent>
+          {views.toString().slice(0, -3) +
+            `,` +
+            views.toString().slice(-3, views.length)}
+        </StatItem>
       </StatsUnit>
       <StatsUnit>
         <StatItem>Likes</StatItem>
-        <StatItem accent>{likes}</StatItem>
+        <StatItem accent>
+          {likes.toString().slice(0, -3) +
+            `,` +
+            likes.toString().slice(-3, likes.length)}
+        </StatItem>
       </StatsUnit>
     </StyledContainer>
   );
+};
+
+Statistic.propTypes = {
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
 };
