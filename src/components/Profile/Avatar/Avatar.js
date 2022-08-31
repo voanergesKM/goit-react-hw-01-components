423de3021ccb77d7c3cb2810417230197cfedf85
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-31 14:10:30 +0300

    User prop-types fixed

--- src/components/Profile/Avatar/Avatar.js
+++ src/components/Profile/Avatar/Avatar.js
@@ -1,5 +1,10 @@
+import PropTypes from 'prop-types';
 import { StyledAvatar } from './Avatar.styled';
 
 export const Avatar = ({ source }) => {
   return <StyledAvatar src={source} />;
+};
+
+Avatar.propTypes = {
+  source: PropTypes.string.isRequired,
 };
--- src/components/Profile/Location/Location.js
+++ src/components/Profile/Location/Location.js
@@ -1,5 +1,10 @@
+import PropTypes from 'prop-types';
 import { StyledLocation } from './Location.styled';
 
 export const UserLocation = ({ location }) => {
   return <StyledLocation>{location}</StyledLocation>;
+};
+
+UserLocation.propTypes = {
+  location: PropTypes.string.isRequired,
 };
