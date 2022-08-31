98631b9ae54766a9d8f6754d32c63babea6d2373
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-31 14:18:34 +0300

    adde forgotten proptypes to user-profile

--- src/components/Profile/Tag/Tag.js
+++ src/components/Profile/Tag/Tag.js
@@ -1,5 +1,10 @@
+import PropTypes from 'prop-types';
 import { StyledTag } from './Tag.styled';
 
 export const Tag = ({ tag }) => {
   return <StyledTag>@{tag}</StyledTag>;
+};
+
+Tag.propTypes = {
+  tag: PropTypes.string.isRequired,
 };
--- src/components/Profile/Title/Title.js
+++ src/components/Profile/Title/Title.js
@@ -1,5 +1,10 @@
+import PropTypes from 'prop-types';
 import { StyledTitle } from './Title.styled';
 
 export const Title = ({ name }) => {
   return <StyledTitle>{name}</StyledTitle>;
+};
+
+Title.propTypes = {
+  name: PropTypes.string.isRequired,
 };
